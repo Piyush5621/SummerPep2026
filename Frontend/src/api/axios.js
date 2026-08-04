93da7b7b08d93067eb725633d4ca32b5965ab694
Piyush <piyushkumar10902080@gmail.com>
2026-08-04 23:30:05 +0530

    Chnage the connection of frontedn and backend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://summerpep2026.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
